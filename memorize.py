37c3fe9cc992755aae1b3be5d1d1fbe743c93be2
Nathaniel Branscum <nathaniel.d.branscum.24@dartmouth.edu>
2022-04-20 12:25:40 -0400

    Removed extraneous and unnecessary global

--- memorize.py
+++ memorize.py
@@ -1,14 +1,15 @@
-# Simple program to memorize a password
+# Simple program to memorize a password or similar
 # Nathaniel Branscum, 4/20/22
 
 from time import sleep
-correct = 0
 
 def set():
     return input("What would you like to memorize?  ")
 
 def main(correct, mem):
     total = 10
+    # Simple, inelegant method to clear screen
+    # Replace later with method that gets screen size
     print("\n" * 20)
     if correct == total:
         print("Congratulations!")
@@ -24,9 +25,10 @@ def main(correct, mem):
         sleep(0.6)
         main(correct - 1, mem)
 
-def start(correct):
+def start():
+    correct = 0
     mem = set()
     main(correct, mem)
 
 
-start(correct)
+start()
